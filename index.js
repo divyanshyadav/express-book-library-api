56938c5129258c59cbdcffe66083e3fe6f038d7b
Divyansh <divyansh.yadav@gmail.com>
2019-05-26 06:33:10 +0530

    set default listening port to 5000 or get it from environment variable PORT if sepecified

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send([1, 2, 3, 4]);
 });
 
-app.listen(5000, () => console.log('Listening on port 5000...'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
