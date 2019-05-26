8e44b0d70c8a5670ebbbee74e6fee14ed3df0ffc
Divyansh <divyansh.yadav@gmail.com>
2019-05-26 07:36:20 +0530

    add some routes

---
+++ data.js
@@ -0,0 +1,7 @@
+const courses = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' },
+];
+
+module.exports = courses;
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express');
+const courses = require('./data');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.json());
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4]);
+    const { search } = req.query;
+    let foundCourses = courses;
+    if (search) {
+        foundCourses = res.send(courses.filter(c => c.name.includes(search)));
+    }
+    res.send(foundCourses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === Number(req.params.id));
+    if (!course) res.status(404).send('Course with given ID is not found.');
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const { name } = req.body;
+
+    if (!name || name.length < 3) {
+        // 400 Bad Request
+        res.status(400).send('Name is required and should be minimum 3 characters long.');
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name,
+    };
+
+    courses.push(course);
+    res.send(course);
 });
 
 const port = process.env.PORT || 5000;
